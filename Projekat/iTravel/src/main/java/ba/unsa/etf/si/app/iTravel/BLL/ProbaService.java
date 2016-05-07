c8bb9a41564b3e2f00974b80f95aba29954d5d19
kprses <kprses1@etf.unsa.ba>
2016-05-07 21:57:13 +0200

    dummy checkin

--- Projekat/iTravel/src/main/java/ba/unsa/etf/si/app/iTravel/BLL/ProbaService.java
+++ Projekat/iTravel/src/main/java/ba/unsa/etf/si/app/iTravel/BLL/ProbaService.java
@@ -1,9 +1,14 @@
 package ba.unsa.etf.si.app.iTravel.BLL;
 
-public class ProbaService {
+import ba.unsa.etf.si.app.iTravel.DAL.DBContext;
 
+public class ProbaService {
+	
+	private DBContext test = new DBContext();
+	
 	public ProbaService() {
 		// TODO Auto-generated constructor stub
+		
 	}
 
 }
