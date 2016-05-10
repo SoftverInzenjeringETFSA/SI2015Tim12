aff4695c06fafc0e8fa871d2d83c3f9e69ebbf31
kprses <kprses1@etf.unsa.ba>
2016-05-11 00:04:20 +0200

    Izbrisano dupliciranje metode PrikaziFormu

--- Projekat/iTravel/src/main/java/ba/unsa/etf/si/app/iTravel/Forms/Klijenti.java
+++ Projekat/iTravel/src/main/java/ba/unsa/etf/si/app/iTravel/Forms/Klijenti.java
@@ -28,7 +28,7 @@ public class Klijenti {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void PrikaziFormu() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
--- Projekat/iTravel/src/main/java/ba/unsa/etf/si/app/iTravel/Forms/KreiranjeKorisnickogRacuna.java
+++ Projekat/iTravel/src/main/java/ba/unsa/etf/si/app/iTravel/Forms/KreiranjeKorisnickogRacuna.java
@@ -48,7 +48,7 @@ public class KreiranjeKorisnickogRacuna {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void PrikaziFormu() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
