2b70be1fac99478b9d4d273a2fa3fd0bd55e4b53
adnatahic <atahic1@etf.unsa.ba>
2016-05-15 18:16:37 +0200

    ocajno

--- Projekat/iTravel/src/test/java/ba/unsa/etf/si/app/iTravel/BLL/SobaTest.java
+++ Projekat/iTravel/src/test/java/ba/unsa/etf/si/app/iTravel/BLL/SobaTest.java
@@ -15,9 +15,9 @@ import ba.unsa.etf.si.app.iTravel.DBModels.Soba;
 public class SobaTest {
 
 	public UnitOfWork uow;
-//	public Soba s;
-//	public Hotel h;
-//	public Destinacija d;
+	public Soba s;
+	public Hotel h;
+	public Destinacija d;
 
 	@Test
 	public void testUbaciSobuUBazu() {
@@ -38,114 +38,115 @@ public class SobaTest {
 		
 	}
 	
-//	
-//	@Test
-//	public void testObrisiSveSobe() {
-//		uow= new UnitOfWork();
-//		//uow.getSobeService().ObrisiSveSobe();
-//	}
-//
-//	@Test
-//	public void testVratiSobaId() {
-//		uow= new UnitOfWork();
-//		PocetneVrijednosti();
-//		List<Soba> sobe= new ArrayList<Soba>();
-//		sobe=uow.getSobeService().VratiSveSobe();
-//		int id= s.getSobaId();
-//		Izbrisi(h,d,s);
-//		assertEquals(sobe.get(sobe.size()-1).getSobaId().intValue(), id);
-//	}
-//
-//	@Test
-//	public void testVratiSveSobe() {
-//		uow= new UnitOfWork();
-//		PocetneVrijednosti();
-//		List<Soba> sobe= new ArrayList<Soba>();
-//		sobe=uow.getSobeService().VratiSveSobe();
-//		
-//		Izbrisi(h,d,s);
-//		assertTrue(sobe.size()>0);
-//	}
-//	
-//
-//	@Test
-//	public void testProvjeriPostojanjeSobaId() {
-//		uow= new UnitOfWork();
-//		PocetneVrijednosti();
-//		assertTrue(uow.getSobeService().ProvjeriPostojanjeSobaId(s.getSobaId()));
-//		Izbrisi(h,d,s);
-//	}
-//
-//	@Test
-//	public void testProvjeriPostojanjeSoba() {
-//		uow= new UnitOfWork();
-//		PocetneVrijednosti();
-//		assertTrue(uow.getSobeService().ProvjeriPostojanjeSoba(s.getBrojKreveta()));
-//		Izbrisi(h,d,s);
-//	}
-//
-//	@Test
-//	public void testAzurirajiliUbaciSobu() {
-//		uow= new UnitOfWork();
-//		PocetneVrijednosti();
-//		Soba s1= new Soba();
-//		s1=s;
-//		s1.setBrojKreveta(5);
-//		uow.getSobeService().AzurirajiliUbaciSobu(s1);
-//		List<Soba> sobe= new ArrayList<Soba>();
-//		sobe= uow.getSobeService().dajSobeZaHotel(h);
-//		assertEquals(5, sobe.get(sobe.size()-1).getBrojKreveta().intValue());
-//		Izbrisi(h,d,s);
-//	}
-//
-//	@Test
-//	public void testDajSobeZaHotel() {
-//		uow= new UnitOfWork();
-//		PocetneVrijednosti();
-//		List<Soba> sobe= new ArrayList<Soba>();
-//		sobe= uow.getSobeService().dajSobeZaHotel(h);
-//		Izbrisi(h,d,s);
-//		assertTrue(sobe.size()>0);
-//	}
-//
-//	@SuppressWarnings("deprecation")
-//	@Test
-//	public void testDajTermineZaSobu() {
-//		uow= new UnitOfWork();
-//		PocetneVrijednosti();
-//		Soba s2= new Soba(h, 5, "opis", 50, 30, null, null);
-//		uow.getSobeService().UbaciSobuUBazu(s2);
-//		ArrayList<Soba> sobe= new ArrayList<Soba>();
-//		sobe.add(s);
-//		sobe.add(s2);
-//		RezervisaniTerminSoba rt= new RezervisaniTerminSoba(null, s, new Date(2016,5,1), new Date(2016,5,10), true);
-//		uow.getRezervisaniTerminService().DodajRezervisaniTermin(rt);
-//		List<RezervisaniTerminSoba> rts= new ArrayList<RezervisaniTerminSoba>();
-//		rts= uow.getSobeService().dajTermineZaSobu(sobe);
-//		uow.getSobeService().ObrisiJenduSobu(s2);
-//		uow.getRezervisaniTerminService().ObrisiJenduSobuRezervacija(rt);
-//		Izbrisi(h,d,s);
-//		assertEquals(1, rts.size());	
-//	}
-//
-//
-//	public void Izbrisi(Hotel h, Destinacija d, Soba s)
-//	{
-//		uow= new UnitOfWork();
-//		uow.getSobeService().ObrisiJenduSobu(s);
-//		uow.getHoteliService().ObrisiJendaHotel(h);
-//		uow.getDestinacijeService().ObrisiJednuDestinaciju(d);
-//	}
-//	
-//	@SuppressWarnings("deprecation")
-//	public void PocetneVrijednosti()
-//	{
-//		d= new Destinacija("mjesto", true, null, null);
-//		uow.getDestinacijeService().KreirajDestinaciju(d);
-//		h= new Hotel(d, "adresa", "drzava", "grad", "061111111", new Date(2016,4,1), new Date(2016,7,1), new Date(2016,4,1), new Date(2016,4,1), "naziv",
-//				"lanac", 5, null, null);
-//		uow.getHoteliService().KreirajHotel(h);
-//		s= new Soba(h, 2, "opis", 50, 30, null, null);
-//		uow.getSobeService().UbaciSobuUBazu(s);
-//	}
+	
+	@Test
+	public void testObrisiSveSobe() {
+		uow= new UnitOfWork();
+		//uow.getSobeService().ObrisiSveSobe();
+	}
+
+	@Test
+	public void testVratiSobaId() {
+		uow= new UnitOfWork();
+		PocetneVrijednosti();
+		List<Soba> sobe= new ArrayList<Soba>();
+		sobe=uow.getSobeService().VratiSveSobe();
+		int id= s.getSobaId();
+		Izbrisi(h,d,s);
+		assertEquals(sobe.get(sobe.size()-1).getSobaId().intValue(), id);
+	}
+
+	@Test
+	public void testVratiSveSobe() {
+		uow= new UnitOfWork();
+		PocetneVrijednosti();
+		List<Soba> sobe= new ArrayList<Soba>();
+		sobe=uow.getSobeService().VratiSveSobe();
+		
+		Izbrisi(h,d,s);
+		assertTrue(sobe.size()>0);
+	}
+	
+
+	@Test
+	public void testProvjeriPostojanjeSobaId() {
+		uow= new UnitOfWork();
+		PocetneVrijednosti();
+		assertTrue(uow.getSobeService().ProvjeriPostojanjeSobaId(s.getSobaId()));
+		Izbrisi(h,d,s);
+	}
+
+	@Test
+	public void testProvjeriPostojanjeSoba() {
+		uow= new UnitOfWork();
+		PocetneVrijednosti();
+		assertTrue(uow.getSobeService().ProvjeriPostojanjeSoba(s.getBrojKreveta()));
+		Izbrisi(h,d,s);
+	}
+
+	@Test
+	public void testAzurirajiliUbaciSobu() {
+		uow= new UnitOfWork();
+		PocetneVrijednosti();
+		Soba s1= new Soba();
+		s1=s;
+		s1.setBrojKreveta(5);
+		uow.getSobeService().AzurirajiliUbaciSobu(s1);
+		List<Soba> sobe= new ArrayList<Soba>();
+		sobe= uow.getSobeService().dajSobeZaHotel(h);
+		assertEquals(5, sobe.get(sobe.size()-1).getBrojKreveta().intValue());
+		Izbrisi(h,d,s);
+	}
+
+	@Test
+	public void testDajSobeZaHotel() {
+		uow= new UnitOfWork();
+		PocetneVrijednosti();
+		List<Soba> sobe= new ArrayList<Soba>();
+		sobe= uow.getSobeService().dajSobeZaHotel(h);
+		Izbrisi(h,d,s);
+		assertTrue(sobe.size()>0);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testDajTermineZaSobu() {
+		uow= new UnitOfWork();
+		PocetneVrijednosti();
+		Soba s2= new Soba(h, 5, "opis", 50, 30, null, null);
+		uow.getSobeService().UbaciSobuUBazu(s2);
+		ArrayList<Soba> sobe= new ArrayList<Soba>();
+		sobe.add(s);
+		sobe.add(s2);
+		RezervisaniTerminSoba rt= new RezervisaniTerminSoba(null, s, new Date(2016,5,1), new Date(2016,5,10), true);
+		uow.getRezervisaniTerminService().DodajRezervisaniTermin(rt);
+		List<RezervisaniTerminSoba> rts= new ArrayList<RezervisaniTerminSoba>();
+		rts= uow.getSobeService().dajTermineZaSobu(sobe);
+		uow.getSobeService().ObrisiJenduSobu(s2);
+		uow.getRezervisaniTerminService().ObrisiJenduSobuRezervacija(rt);
+		Izbrisi(h,d,s);
+		assertEquals(1, rts.size());	
+	}
+
+
+	public void Izbrisi(Hotel h, Destinacija d, Soba s)
+	{
+		uow= new UnitOfWork();
+		uow.getSobeService().ObrisiJenduSobu(s);
+		uow.getHoteliService().ObrisiJendaHotel(h);
+		uow.getDestinacijeService().ObrisiJednuDestinaciju(d);
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void PocetneVrijednosti()
+	{
+		d= new Destinacija("mjesto", true, null, null);
+		uow.getDestinacijeService().KreirajDestinaciju(d);
+		h= new Hotel(d, "adresa", "drzava", "grad", "061111111", new Date(2016,4,1), new Date(2016,7,1), new Date(2016,4,1), new Date(2016,4,1), "naziv",
+				"lanac", 5, null, null);
+		uow.getHoteliService().KreirajHotel(h);
+		s= new Soba(h, 2, "opis", 50, 30, null, null);
+		uow.getSobeService().UbaciSobuUBazu(s);
+	}
+
 }
