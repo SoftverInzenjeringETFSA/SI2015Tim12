f2e09d219572693a664a77b2541abf19a58917fb
srepuh <srepuh1@etf.unsa.ba>
2016-05-15 22:52:00 +0200

    radi saaaad

--- Projekat/iTravel/src/test/java/ba/unsa/etf/si/app/iTravel/BLL/PostavkeTest.java
+++ Projekat/iTravel/src/test/java/ba/unsa/etf/si/app/iTravel/BLL/PostavkeTest.java
@@ -2,6 +2,8 @@ package ba.unsa.etf.si.app.iTravel.BLL;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import ba.unsa.etf.si.app.iTravel.DAL.DBContext;
@@ -14,20 +16,21 @@ public class PostavkeTest {
 	@Test
 	public void testModulOmoguce(){
 		uow= new UnitOfWork();
-		Postavke p=new Postavke(1000,"Hoteli", true);
+		Postavke p=new Postavke(1001,"Hoteli", true);
 		p= baza.getPostavkeRepository().spasiUBazu(p);
-		assertTrue(uow.getPostavkeService().modulOmogucen(1000));
+		boolean provjera=uow.getPostavkeService().modulOmogucen(1001);
 		baza.getPostavkeRepository().obrisiIzBaze(p);
+		assertTrue(provjera);
 	}
 	
 	@Test
 	public void testSpasiPromjenePostavki(){
 		uow=new UnitOfWork();
-		Postavke p=new Postavke(1400, "Hoteli", true);
+		Postavke p=new Postavke(1401, "Hoteli", true);
 		p= uow.getPostavkeService().baza.getPostavkeRepository().spasiUBazu(p);
-	    Postavke p1=new Postavke(1500, "Sobe", false);
+	    Postavke p1=new Postavke(1501, "Sobe", false);
 		p1= uow.getPostavkeService().baza.getPostavkeRepository().spasiUBazu(p1);
-		Postavke p2=new Postavke(1600, "Rezervacije", true);
+		Postavke p2=new Postavke(1601, "Rezervacije", true);
 		p2= uow.getPostavkeService().baza.getPostavkeRepository().spasiUBazu(p2);
 		
 		boolean[] nizPostavki = new boolean[3];
@@ -35,10 +38,13 @@ public class PostavkeTest {
 		nizPostavki[1] = true;
 		nizPostavki[2] = false;
 		uow.getPostavkeService().spasiPromjenePostavki(nizPostavki);
-		assertFalse(uow.getPostavkeService().dajSvePostavke()[0]);
+		boolean provjera=uow.getPostavkeService().dajSvePostavke()[0];
+		
 		baza.getPostavkeRepository().obrisiIzBaze(p);
 		baza.getPostavkeRepository().obrisiIzBaze(p1);
 		baza.getPostavkeRepository().obrisiIzBaze(p2);
+		assertFalse(provjera);
+		
 	}
 	
 	@Test
@@ -51,10 +57,12 @@ public class PostavkeTest {
 		Postavke p2=new Postavke(1900, "Rezervacije", true);
 		p2= uow.getPostavkeService().baza.getPostavkeRepository().spasiUBazu(p2);
 		
-		assertEquals(3,baza.getPostavkeRepository().ucitajSveIzBaze());
+		List<Postavke> lista=baza.getPostavkeRepository().ucitajSveIzBaze();
 		baza.getPostavkeRepository().obrisiIzBaze(p);
 		baza.getPostavkeRepository().obrisiIzBaze(p1);
 		baza.getPostavkeRepository().obrisiIzBaze(p2);
+		
+		assertEquals(3,lista.size());
 	}
 	
 }
