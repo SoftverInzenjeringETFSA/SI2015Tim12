aeb3d8bfccc990067d148f6c99121cac81801518
adnatahic <atahic1@etf.unsa.ba>
2016-05-15 14:35:36 +0200

    izmjene

---
+++ Projekat/iTravel/src/test/java/ba/unsa/etf/si/app/iTravel/BLL/SobaTest.java
@@ -0,0 +1,71 @@
+package ba.unsa.etf.si.app.iTravel.BLL;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SobaTest {
+
+	public UnitOfWork uow;
+
+	@Test
+	public void testUbaciSobuUBazu() {
+		
+	}
+
+	@Test
+	public void testObrisiJenduSobu() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testObrisiSveSobe() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testVratiSobaId() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testVratiSveSobe() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testVrtatiSobuPoHoteluIBrojuKreveta() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testProvjeriPostojanjeSobaId() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testProvjeriPostojanjeSoba() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAzurirajiliUbaciSobu() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDajSobeZaHotel() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDajTermineZaSobu() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDajSlobodneSobeZaHotel() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+}
